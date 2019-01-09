22e7c19d298120dcff77cdad52ecf864262c4404
gemary <ducdang12k11@viendong.edu.vn>
2019-01-09 23:10:38 +0700

    new

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ function MoveYes()
 }
 function EventbtnYes()
 {
-    document.getElementById("demo").innerHTML ="Nếu cảm thấy mình bị ép buộc thì im luôn há Còn không thì Nói Mị 1 tiếng!";
-
+  document.getElementById("demo1").innerHTML = "Còn không thì Nói Mị 1 tiếng!";
+    document.getElementById("demo").innerHTML ="Nếu cảm thấy mình bị ép buộc thì im luôn há";
 }
