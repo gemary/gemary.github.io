634106f2fe52aa82650ccc771581e131721ababd
gemary <ducdang12k11@viendong.edu.vn>
2019-01-09 22:37:44 +0700

    new

---
+++ main.js
@@ -0,0 +1,9 @@
+function MoveYes()
+{
+    var ValueTop =document.getElementById("btnNo").style.top = (Math.random() * ( 90 -1 ) +1 ) +"%";
+    var Valueleft = document.getElementById("btnNo").style.left = (Math.random() * ( 90 -1 ) +1 ) +"%";
+}
+function EventbtnYes()
+{
+    
+}
