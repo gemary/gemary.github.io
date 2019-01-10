97eb3a999f2c4f916e4a40305410ed8c23172762
gemary <ducdang12k11@viendong.edu.vn>
2019-01-10 20:45:19 +0700

    new

--- mylove/main.js
+++
@@ -1,9 +0,0 @@
-function MoveYes()
-{
-    var ValueTop =document.getElementById("btnNo").style.top = (Math.random() * ( 90 -1 ) +1 ) +"%";
-    var Valueleft = document.getElementById("btnNo").style.left = (Math.random() * ( 90 -1 ) +1 ) +"%";
-}
-function EventbtnYes()
-{
-    
-}
