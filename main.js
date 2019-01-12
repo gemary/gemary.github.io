00424e9b660724bdffca69f8f34e35207950e791
gemary <ducdang12k11@viendong.edu.vn>
2019-01-12 22:37:15 +0700

    ex

--- main.js
+++
@@ -1,9 +0,0 @@
-function MoveYes()
-{
-    var ValueTop =document.getElementById("btnNo").style.top = (Math.random() * ( 90 -1 ) +1 ) +"%";
-    var Valueleft = document.getElementById("btnNo").style.left = (Math.random() * ( 90 -1 ) +1 ) +"%";
-}
-function EventbtnYes()
-{
-    document.getElementById("demo").innerHTML = "I Love You Too";
-}
