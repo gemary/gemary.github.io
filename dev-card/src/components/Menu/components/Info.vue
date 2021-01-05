10f9d46c40632f8b0a151944d9e5837d5e74063e
gemary <ducdang1206@gmail.com>
2021-01-05 22:05:35 +0700

    add svg, add scss file

---
+++ dev-card/src/components/Menu/components/Info.vue
@@ -0,0 +1,23 @@
+<template>
+  <a href="#">
+      <h1>Gemary</h1>
+  </a>
+  <div class="profile-img">
+      <img src="https://avatars0.githubusercontent.com/u/42095121?s=460&u=e55434246ae9f2c83c3113c4fd47084f5614c8bf&v=4" alt="avatar">
+      <div class="bio">
+          <p>Hi, my name is Gemary Doe and I'm a Junior software engineer. Welcome to my personal website!</p>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Info extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
